389c52cbbbefa1874601723ea514f9b186f561ef
Carl Crott <carlcrott@gmail.com>
2014-11-12 18:26:35 -0800

    edits for build minions

--- build_minions.sh
+++ build_minions.sh
@@ -1,16 +1,9 @@
+
 #!/bin/bash
 ## Example:
 #./build_minions.sh 5 testinstance
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    echo "run script like:"
-    echo "./build_minions.sh 5 testinstance"
-    exit
-fi
-
-echo openstack_8gb: > cloud.map
+echo all_settings: > cloud.map
 #TODO: clean out all existing keys listed via:
 # salt-key -L
 
@@ -20,24 +13,12 @@ done
 
 echo "${2}" > minion_name
 
-# possible replacement:
-# service salt-master stop && service salt-master start
-pkill salt-master && salt-master -d
-
-# echo start time of instancing event
-echo "${2}" >> stats
-echo date >> stats
-
 ## Deploy instances
-yes | sudo salt-cloud -P -m cloud.map -l all
+yes | sudo salt-cloud -P -m cloud.map
 
-# echo finish time of instancing
-echo date >> stats
 
 # write out configs for general usage / parsing
-salt '*' network.ip_addrs --out=json --static > minion_metadata.json
-
-
+#salt '*' network.ip_addrs --out=json --static > minion_metadata.json
 
 # once the mpi list is built out pull it onto all the minions
 #salt '*' state.highstate -l debug
@@ -49,7 +30,7 @@ salt '*' network.ip_addrs --out=json --static > minion_metadata.json
 # salt '*' cmd.run 'uname -a'
 
 ## install dependencies
-salt '*' state.highstate -l all
+#salt '*' state.highstate -l all
 #salt-call -l debug state.highstate
 
 
